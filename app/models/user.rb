37a5632c5721067fd2726b838c418fcc90f2239d
matildeyang <91755817+matildeyang@users.noreply.github.com>
2023-12-05 02:49:09 +0000

    confused

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@
 # Table name: users
 #
 #  id                     :integer          not null, primary key
-#  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
@@ -15,7 +14,6 @@
 #
 # Indexes
 #
-#  index_users_on_email                 (email) UNIQUE
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #
 class User < ApplicationRecord
--- db/migrate/20231205023658_remove_email_from_user.rb
+++
@@ -1,5 +0,0 @@
-class RemoveEmailFromUser < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :user, :email
-  end
-end
